d95dd341e5d87c95ffba0c50e9375a82ed525ab4
Valbrand <daniel@dvalbrand.com>
2015-10-10 16:32:34 -0300

    Added static files service

--- src/index.js
+++ src/index.js
@@ -2,16 +2,40 @@ var Hapi = require('hapi');
 var Good = require('good');
 var GoodConsole = require('good-console');
 var Vision = require('vision');
+var Inert = require('inert');
 var Handlebars = require('handlebars');
 var HandlebarsLayouts = require('handlebars-layouts');
+var Path = require('path');
 
 var router = require('./routes.js');
 
 var engine = Handlebars.create();
 HandlebarsLayouts.register(engine);
 
-var server = new Hapi.Server();
+var server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, '../public')
+      }
+    }
+  }
+});
 server.connection({ port: 3000 });
+
+server.register(Inert, function(err) {
+  if (err) throw err;
+
+  server.route({
+    method: 'GET',
+    path: '/static/{filepath*}',
+    handler: {
+      file(request) {
+        return request.params.filename;
+      }
+    }
+  })
+})
 
 server.register(Vision, function(err) {
   if (err) throw err;
@@ -20,9 +44,9 @@ server.register(Vision, function(err) {
     engines: {
       html: engine
     },
-    relativeTo: __dirname,
-    path: '../templates/pages',
-    partialsPath: '../templates/layouts'
+    relativeTo: Path.join(__dirname, '../views'),
+    path: 'pages',
+    partialsPath: 'layouts'
   });
 });
 
