b137a0e6142ee463f0d5b2bcb595b5ac489380ba
Valbrand <daniel@dvalbrand.com>
2015-10-11 18:44:41 -0300

    Erased useless comments

--- src/model/data/partnerCompany/entities/PartnerCompany.ts
+++ src/model/data/partnerCompany/entities/PartnerCompany.ts
@@ -25,25 +25,5 @@ class PartnerCompany {
     }
 
 }
-/*
- // Schema
- var PartnerCompanySchema = new mongoose.Schema({
 
- name: { required: true, type: String },
- branch: { required: true, type: String },
- background: { required: true, type: String },
- support: { required: true, type: String },
- support_proposal: { required: true, type: String },
- partnershipType: { required: true, type: String },
- address: { required: true, type: Address}
-
- })
-
- // Interface for the mongoose
- //export interface PartnerCompanyDocument extends PartnerCompany, mongoose.Document { }
-
- // Model
- //export var PartnerCompanyModel = mongoose.model<PartnerCompanyDocument>('PartnerCompany', schema)
- */
-// Export Class
 export = PartnerCompany;
