98b47e67acd6b97c1f68ebc989a361d8ebd31381
Valbrand <daniel@dvalbrand.com>
2015-10-12 10:16:00 -0300

    Added some routes to config file

--- src/routes.js
+++ src/routes.js
@@ -15,11 +15,41 @@ function configureRoutes(server) {
   });
 
   // Regular routes for the application
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: controller.home
-	});
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: controller.home
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/login',
+    handler: controller.home // TODO: criar o método no controller
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/logout',
+    handler: controller.home // TODO: criar o método no controller
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/profile',
+    handler: controller.home // TODO: criar o método no controller
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/companies/new',
+    handler: controller.home // TODO: criar o método no controller
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/companies/search',
+    handler: controller.home // TODO: criar o método no controller
+  });
 }
 
 module.exports = {
